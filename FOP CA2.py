3efe974b477aa2e928b6835ab4b945e613e094b9
rnviv <hjtoh.25@ichat.sp.edu.sg>
2026-01-16 11:48:35 +0800

    Add files via upload

---
+++ FOP CA2.py
@@ -0,0 +1,39 @@
+#Program needs to remember changes = use chapter 6 
+
+line = '---------------------------------------------------------------------------'
+print(line,'\n'+'  Class: 01\n  1.Toh Heng Jui\n  2.Lexiann\n'+line)
+print('             Cryptocurrency Portfolio Application Main Menu')
+print(line)
+print('1. Display Cryptocurrency\n2. Add Cryptocurrency\n3. Amend Cryptocurrency\n4. Remove Crytocurrency\n5. Cryto Porfolio Statement\n6. HJ function\n7. Lexiann function\nE. Exit Main Menu')
+print (line)
+option = input('Select an option: ')
+while (True):
+    if option =='1':
+        
+    
+                
+
+        break
+    elif option =='2':
+        print ('2')
+        break
+    elif option =='3':
+        print ('3')
+        break
+    elif option == '4':
+        print ('4')
+        break
+    elif option =='5':
+        print ('5')
+        break
+    elif option == '6':
+        print ('6')
+        break
+    elif option =='7':
+        print ('7')
+        break
+    elif option.upper()== 'E':
+        print('You have chosen to exit')
+        break
+
+#hj function - find live data last 6 months etc put into a graph and compare 2 different coins
